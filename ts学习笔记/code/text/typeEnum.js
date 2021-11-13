11aaf50c060deec2b3fbdb7a2d5e071914f351ea
jinxudong <18751241086@163.com>
2021-11-14 01:03:10 +0800

    ts学习

---
+++ ts学习笔记/code/text/typeEnum.js
@@ -0,0 +1,8 @@
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var colorName = Color[2];
+console.log(colorName); // 显示'Green'因为上面代码里它的值是2
---
+++ ts学习笔记/code/text/typeEnum.ts
@@ -0,0 +1,4 @@
+enum Color {Red = 1, Green, Blue}
+let colorName: string = Color[2]
+
+console.log(colorName)  // 显示'Green'因为上面代码里它的值是2
